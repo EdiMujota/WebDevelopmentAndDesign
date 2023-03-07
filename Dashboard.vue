2c96de88fbb24b72c6fd7aa163737ff8225aaa68
EdiMujota <93375392+EdiMujota@users.noreply.github.com>
2023-03-07 19:33:09 +0100

    Admin Panel - Home Page

---
+++ Dashboard.vue
@@ -0,0 +1,70 @@
+<template>
+  <Sidebar fixed="top" />
+  <router-view name="admin" />
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>E-Commerce</title>
+      <link rel="stylesheet" href="./css/all.css" />
+      <link rel="stylesheet" href="./css/style.css" />
+    </head>
+    <body>
+      <div class="container">
+        <header>
+          <!-- Carrotsuite navigation -->
+          <nav class="carrotsuite-nav">
+            <div class="logo">
+              <a href="index.html" class="active nav-item">
+                <img src="./images/e-commerce.png" alt="Images" />
+              </a>
+            </div>
+            <br />
+            <div class="user" id="user">
+              <h2>ADMIN</h2>
+              <div class="user-img-wrapper">
+                <img src="./images/useravatar.png" alt="User 1" />
+              </div>
+            </div>
+          </nav>
+          <!-- end of feeds navigation -->
+        </header>
+        <main class="main">
+          <section class="sidebar">
+            <h3>
+              This panel created for admin. Here you can modify with the
+              application!
+            </h3>
+          </section>
+        </main>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue";
+
+export default {
+  components: { Sidebar },
+  data: function () {
+    return {};
+  },
+};
+
+const menuLinks = document.querySelectorAll(".sidebar .menu a");
+
+for (const link of menuLinks) {
+  link.addEventListener("mouseenter", function () {
+    if (window.matchMedia("(max-width: 626px)").matches) {
+      const tooltip = this.querySelector("span").textContent;
+      this.setAttribute("title", tooltip);
+    } else {
+      this.removeAttribute("title");
+    }
+  });
+}
+</script>
+
+<style scoped></style>
---
+++ Sidebar.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container-fluid">
+      <router-link to="/dashboard" class="link-light text-decoration-none"
+        ><h3>Administrator</h3></router-link
+      >
+      <div
+        class="collapse navbar-collapse justify-content-center"
+        id="navbarCenteredExample"
+      >
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link
+              to="/createcomputers"
+              class="nav-link active text-light"
+              aria-current="page"
+              >Computers</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="/createllaptops"
+              class="nav-link active text-light"
+              aria-current="page"
+              >Laptops</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="/createphones"
+              class="nav-link active text-light"
+              aria-current="page"
+              >Phones
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="createtvs"
+              class="nav-link active text-light"
+              aria-current="page"
+              >TVs</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="createoutlets"
+              class="nav-link active text-light"
+              style=""
+              aria-current="page"
+              >Outlets</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div></div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.nav-link {
+  font-size: 1.3rem;
+  border-radius: 2rem;
+  margin-right: 2rem;
+  transition: 0.5s;
+}
+.nav-link:hover {
+  background-color: white;
+  color: rgb(97, 97, 97) !important;
+  border-radius: 1rem;
+}
+</style>
